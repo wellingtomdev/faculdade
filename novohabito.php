28e0c142cf97c2fba8b4bcd0721a54ecff3992c3
Wellington <wellingtomdev@gmail.com>
2022-11-23 08:23:53 -0300

    Revover estilização antiga

--- novohabito.php
+++ novohabito.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles.css">
     <title>Lista de hábitos</title>
 </head>
 
