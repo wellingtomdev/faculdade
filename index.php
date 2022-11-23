ec1437a78d83534ec2bc94c9f0a4c5fc510fb854
Wellington <wellingtomdev@gmail.com>
2022-11-23 08:21:31 -0300

    Adicionando bootstrap

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Lista de hábitos</title>
 </head>
 
@@ -67,7 +68,7 @@
             <p>Você não possui hábitos cadastrados!</p>
             <p>Começe já a mudar sua vida!</p>
         <?php
-        } 
+        }
         // fim do if
         // Fecha a conexão com o MySQL
         $conexao->close();
@@ -75,6 +76,7 @@
 
         <a href="novohabito.php">Cadastrar Hábito</a>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
